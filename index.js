e6e485cb88c2c2fee180b5a1b0e409bba282fc69
Dhanushmoorthyr <sec21ee094@sairamtap.edu.in>
2024-07-07 13:48:39 +0530

    Do update

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function rgen(a, b) {
 
 const obtainValues = (rb) => {
   return { 
-    DO: rgen(5.9, 6.5).toFixed(2), 
+    DO: rgen(6.1, 6.8).toFixed(2), 
     Temp: rgen(29.4,31.3).toFixed(2), 
     pH: parseFloat(rb.pH) == 0? rgen(7.4,7.7).toFixed(2): rb.pH, 
     Conduct: rgen(31, 33).toFixed(2)
